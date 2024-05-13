bb61705ca85d21460d11240aec2fb1bafa6e2d9e
AbdulelahSA <abdulelah.alian@gmail.com>
2024-05-14 02:39:05 +0300

    Update SignUp(email user first&last name) & add "update name" page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
-from .views import SignUpView
-
+from .views import SignUpView, update_name
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('update_name/', update_name, name='update_name'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
+from django.contrib.auth.models import User
 from django.views.generic import CreateView
-
+from django.urls import reverse_lazy
+from .forms import SignUpForm
+from django import forms
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .forms import UpdateNameForm
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
+    model = User
+    form_class = SignUpForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
+
+
+@login_required
+def update_name(request):
+    if request.method == 'POST':
+        form = UpdateNameForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('myprofile')
+    else:
+        form = UpdateNameForm(instance=request.user)
+    return render(request, 'registration/update_name.html', {'form': form})
