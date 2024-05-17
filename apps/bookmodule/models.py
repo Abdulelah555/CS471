8fe242e1b38296ca87a74a8e58f5cc320d7ff1a2
AbdulelahSA <abdulelah.alian@gmail.com>
2024-05-18 02:27:27 +0300

    add login_required

--- apps/bookmodule/models.py
+++ apps/bookmodule/models.py
@@ -1,21 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Task(models.Model):
-    title = models.CharField(max_length = 50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
     createdate = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField()
-    Priority = models.CharField(max_length = 50)
-    State = models.CharField(max_length = 50)
-    description = models.CharField(max_length = 500)
+    Priority = models.CharField(max_length=50)
+    State = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
 
-    
 class CheckList(models.Model):
-    task = models.ForeignKey(
-        Task,
-        on_delete=models.PROTECT,
-        blank=False
-        )
-    title = models.CharField(max_length = 50)
-    description = models.CharField(max_length = 500)
-    done = models.CharField(max_length = 50,default=0)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    done = models.CharField(max_length=50, default=0)
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,88 +1,92 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task, CheckList
 from .forms import TaskForm, CheckListForm
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
-def index(request):
-# render the appropriate template for this request
 
+def index(request):
     return render(request, 'bookmodule/index.html')
 
+@login_required
 def myprofile(request):
-# render the appropriate template for this request
-
     return render(request, 'registration/myprofile.html')
+
+@login_required
 def tasks(request):
-# render the appropriate template for this request
-    Tasks = Task.objects.all()
-    
-    return render(request, 'bookmodule/tasks.html',{'Tasks': Tasks})
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'bookmodule/tasks.html', {'Tasks': tasks})
 
-
+@login_required
 def task(request, tId):
-    task = Task.objects.get(id=tId)
+    task = Task.objects.get(id=tId, user=request.user)
     checklist_count = CheckList.objects.filter(task=task).count()
-    checklist_done_count = CheckList.objects.filter(task=task,done="1").count()
-    checklist = CheckList.objects.filter(task = task)
+    checklist_done_count = CheckList.objects.filter(task=task, done="1").count()
+    checklist = CheckList.objects.filter(task=task)
+    return render(request, 'bookmodule/task.html', {'task': task, 'checklist': checklist, 'checklist_count': checklist_count, 'checklist_done_count': checklist_done_count})
 
-    return render(request, 'bookmodule/task.html', {'task': task, 'checklist': checklist,'checklist_count':checklist_count,'checklist_done_count':checklist_done_count})
-
-
+@login_required
 def create(request):
-# render the appropriate template for this request
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
-            obj = form.save()
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
             return redirect('tasks')
-    form = TaskForm(None)
-    return render(request, 'bookmodule/create.html',{'form':form})
+    form = TaskForm()
+    return render(request, 'bookmodule/create.html', {'form': form})
 
-def edit(request,tId):
-# render the appropriate template for this request
-    task = Task.objects.get(id = tId)
+@login_required
+def edit(request, tId):
+    task = Task.objects.get(id=tId, user=request.user)
     if request.method == 'POST':
         form = TaskForm(request.POST, instance=task)
         if form.is_valid():
-            task.save()
-            return redirect('task', tId = task.id )
+            form.save()
+            return redirect('task', tId=task.id)
     form = TaskForm(instance=task)
-    return render(request, 'bookmodule/edit.html', {'form':form})
+    return render(request, 'bookmodule/edit.html', {'form': form})
 
+@login_required
 def delete(request, tId):
-    task = get_object_or_404(Task, id=tId)
+    task = get_object_or_404(Task, id=tId, user=request.user)
     if request.method == 'POST':
         task.delete()
         return redirect('tasks')
     return render(request, 'bookmodule/delete.html', {'task': task})
 
     
-def createchecklist(request,tId):
-# render the appropriate template for this request
+@login_required
+def createchecklist(request, tId):
+    task = Task.objects.get(id=tId, user=request.user)
     if request.method == 'POST':
         form = CheckListForm(request.POST)
         if form.is_valid():
-            obj = form.save(commit=False)
-            obj.task = Task.objects.get(id=tId)  
-            obj.save()
-            return redirect('task', tId = tId)
-    form = CheckListForm(None)
-    return render(request, 'bookmodule/createchecklist.html',{'form':form})
+            checklist = form.save(commit=False)
+            checklist.task = task
+            checklist.save()
+            return redirect('task', tId=tId)
+    form = CheckListForm()
+    return render(request, 'bookmodule/createchecklist.html', {'form': form})
 
 
-def editchecklist(request,tId,cId):
-    checklist = CheckList.objects.get(id = cId)
+@login_required
+def editchecklist(request, tId, cId):
+    task = Task.objects.get(id=tId, user=request.user)
+    checklist = CheckList.objects.get(id=cId, task=task)
     if request.method == 'POST':
         form = CheckListForm(request.POST, instance=checklist)
         if form.is_valid():
-            checklist.save()
-            return redirect('task', tId = tId)
+            form.save()
+            return redirect('task', tId=tId)
     form = CheckListForm(instance=checklist)
-    return render(request, 'bookmodule/editchecklist.html', {'form':form})
+    return render(request, 'bookmodule/editchecklist.html', {'form': form})
 
+@login_required
 def delete_checklist(request, tId, cId):
     checklist_item = get_object_or_404(CheckList, id=cId)
     if request.method == 'POST':
         checklist_item.delete()
         return redirect('task', tId=tId)
     return render(request, 'bookmodule/delete_checklist.html', {'checklist_item': checklist_item, 'task_id': tId})
+
